f0ac11ecf35ab36e617d6bda470148eb95df168b
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-27 14:43:18 +0100

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+""" floor function module """
+
+
+def floor(n: float) -> int:
+    """Takes a float and returns the floor of the float"""
+    return math.floor(n)
