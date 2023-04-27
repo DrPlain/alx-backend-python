e403d1258b5b636b92ce804c171e12202291f4a0
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-27 15:02:05 +0100

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Annotation of list """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ Takes input_list of floats and returns their sum as a float"""
+    return sum(input_list)
