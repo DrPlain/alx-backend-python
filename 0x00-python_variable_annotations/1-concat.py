4e96ade7c54af0eeaa5e54953d28b9f787d79ebc
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-27 14:34:56 +0100

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" concat function module """
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two strings and returns a concatenated string"""
+    str1 += str2
+    return str1
