655cc8b2ac9fe684d24e629342f7409aaf801018
Gideon Obiasor <gideonoiasor@gmail.com>
2023-04-27 15:14:25 +0100

    Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Mixed list typing """
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Takes a mixed list of integers and float and returns their sum as float"""
+    return sum(mxd_lst)
