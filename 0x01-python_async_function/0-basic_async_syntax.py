c3d2af8b18c80cc6f58d023add179cb0d0adf442
Gideon Obiasor <gideonoiasor@gmail.com>
2023-05-02 12:58:42 +0100

    Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,6 @@ import random
 async def wait_random(max_delay: int=10) -> float:
     """Basic async coroutine"""
     # uniform returns a floating num between two specified nums both included
-    num = random.uniform(1, max_delay)
+    num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
