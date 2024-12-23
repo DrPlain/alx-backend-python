7f3ceaf9c9b662529c8f04b3094db00771b5426b
Gideon Obiasor <gideonoiasor@gmail.com>
2024-12-23 20:22:52 +0100

    Configured rest_framework

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -25,4 +25,20 @@ class UserSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
 
 
-def MessageSerializer(serializers)
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        read_only_fields = ['message_id', 'sent_at']
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    messages = MessageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+        read_only_fields = ['conversation_id', 'created_at']
