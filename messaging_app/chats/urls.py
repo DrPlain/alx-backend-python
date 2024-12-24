df199d6cb17720045a9d5625ebb1e9015d2820c0
Gideon Obiasor <gideonoiasor@gmail.com>
2024-12-24 13:30:23 +0100

    Added nested routers to my views

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,21 @@
 from rest_framework import routers
+from rest_framework_nested.routers import NestedDefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r'conversation', ConversationViewSet)
-router.register(r'message', MessageViewSet)
-router.register(r'user', UserViewSet)
+router.register(r'conversations', ConversationViewSet,
+                basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
+router.register(r'users', UserViewSet, 'users')
 
-urlpatterns = router.urls
+# Nested router for messages within a conversation
+nested_router = NestedDefaultRouter(
+    router, r'conversations', lookup='conversation_id')
+nested_router.register(r'messages', MessageViewSet,
+                       basename='conversation-messages')
+
+urlpatterns = []
+urlpatterns += router.urls
+urlpatterns += nested_router.urls
