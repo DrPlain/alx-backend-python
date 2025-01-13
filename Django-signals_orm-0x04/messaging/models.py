85a92f6b9024d116b598f5d939636e5c37bad5c2
Gideon Obiasor <gideonoiasor@gmail.com>
2025-01-13 21:51:47 +0100

    Worked on pre-save signal

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from chats.models import User
 
@@ -8,6 +9,8 @@ class Message(models.Model):
     receiver = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
+    # Tracks if the message has been edited
+    edited = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -26,3 +29,27 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user} - {self.notification_type} - Read: {self.is_read}"
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # Tracks if the message has been edited
+    edited = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.receiver} at {self.timestamp}"
+
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(
+        Message, on_delete=models.CASCADE, related_name='edit_history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History for Message ID {self.message.id} at {self.edited_at}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from messaging.models import Message, Notification
+from messaging.models import Message, Notification, MessageHistory
 
 
 @receiver(post_save, sender=Message)
@@ -10,3 +10,16 @@ def message_post_save_handler(sender, instance: Message, created, **kwargs):
             user=instance.receiver,
             message=instance,
         )
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance: Message, **kwargs):
+    if instance.pk:
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_message, old_content=old_message.content)
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
