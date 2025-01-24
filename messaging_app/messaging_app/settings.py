85ea7da97f135f9d5ef4e9c5e9c0c102fe0a8e38
Gideon Obiasor <gideonoiasor@gmail.com>
2025-01-24 22:29:19 +0100

    Implemented blue-green deployment strategy

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
-    'chats.middleware.RestrictAccessByTimeMiddleware',
+    # 'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.RestrictNumberOfPostMessages',
 ]
 
